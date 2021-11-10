2ceb71cec409d0f9afd4d68c775991a083f8bcd9
hjforbes <63114804+hjforbes@users.noreply.github.com>
2021-11-10 19:58:48 +0000

    add covarites

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,22 +1,39 @@
-from cohortextractor import codelist, codelist_from_csv
+from cohortextractor import codelist, codelist_from_csv, combine_codelists
 
 # OUTCOME CODELISTS
-covid_identification_in_primary_care_case_codes_clinical = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
-    system="ctv3",
-    column="CTV3ID",
+covid_codes = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
 )
 
-covid_identification_in_primary_care_case_codes_test = codelist_from_csv(
+covid_primary_care_positive_test = codelist_from_csv(
     "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
     system="ctv3",
     column="CTV3ID",
 )
-
-covid_identification_in_primary_care_case_codes_seq = codelist_from_csv(
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+covid_primary_care_sequalae = codelist_from_csv(
     "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
     system="ctv3",
     column="CTV3ID",
+)
+any_primary_care_code = combine_codelists(
+    covid_primary_care_code,
+    covid_primary_care_positive_test,
+    covid_primary_care_sequalae,
+)
+
+#FOLLOW-UP VARIABLES
+## First COVID vaccination administration in EMIS
+covid_vaccine_EMIS_codes = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-covadm1.csv",
+    system = "snomed",
+    column = "code",
 )
 
 ## Chronic Liver disease codes
@@ -24,4 +41,41 @@ chronis_liver_disease_codes = codelist_from_csv(
   "codelists/primis-covid19-vacc-uptake-cld.csv",
   system = "snomed",
   column = "code",
+)
+
+opensafely_ethnicity_codes_16 = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_16",
+)
+
+primis_covid19_vacc_update_ethnicity = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-eth2001.csv",
+    system="snomed",
+    column="code",
+    category_column="grouping_16_id",
+)
+
+smoking_clear = codelist_from_csv(
+    "codelists/opensafely-smoking-clear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+smoking_unclear = codelist_from_csv(
+    "codelists/opensafely-smoking-unclear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+dementia_codes = codelist_from_csv(
+    "codelists/opensafely-dementia-complete.csv", system="ctv3", column="code"
+)
+bmi_codes = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-bmi.csv",
+    system = "snomed",
+    column = "code",
 )
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -19,12 +19,11 @@ from cohortextractor import (
 
 ## Import codelists from codelist.py (which pulls them from the codelist folder)
 from codelists import *
+import study_def_helper_functions as helpers
 
 # --- DEFINE STUDY POPULATION ---
 
 ## Define study start and end variables explicitly
-
-
 study = StudyDefinition(
     index_date = "2020-01-01",
 
@@ -71,10 +70,134 @@ study = StudyDefinition(
     
   ),
 
-## DEMOGRAPHIC INFORMATION
+##NB exclusions not written into study definition
+
+
+# --- DEFINE COVID-19 EXPOSURE VARIABLES ---
+
+     # Record COVID-19 infection date
+    sgss_covid19_date=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        returning="date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        on_or_after="index_date",
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "uniform",
+            "incidence": 0.05,
+        },
+    ),
+    primary_care_covid19_date=patients.with_these_clinical_events(
+        combine_codelists(
+            covid_primary_care_code,
+            covid_primary_care_positive_test,
+            covid_primary_care_sequalae,
+        ),
+        returning="date",
+        on_or_after="index_date",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "uniform",
+            "incidence": 0.05,
+        },
+    ),
+    hospital_covid19_date=patients.admitted_to_hospital(
+        with_these_diagnoses=covid_codes,
+        returning="date_admitted",
+        on_or_after="index_date",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "uniform",
+            "incidence": 0.05,
+        },
+    ),
+    death_covid19_date=patients.with_these_codes_on_death_certificate(
+        covid_codes,
+        returning="date_of_death",
+        on_or_after="index_date",
+        match_only_underlying_cause=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "uniform",
+            "incidence": 0.02
+        },
+    ),
+    exp_confirmed_covid19_date=patients.minimum_of(
+        "sgss_covid19_date","primary_care_covid19_date","hospital_covid19_date","death_covid19_date"
+    ),
+
+# --- DEFINE FOLLOW-UP VARIABLES
+
+    # Record death date
+    primary_care_death_date=patients.with_death_recorded_in_primary_care(
+        on_or_after="index_date",
+        returning="date_of_death",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "exponential_increase",
+        },
+    ),
+    ons_died_from_any_cause_date=patients.died_from_any_cause(
+        on_or_after="index_date",
+        returning="date_of_death",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "index_date", "latest" : "today"},
+            "rate": "exponential_increase",
+        },
+    ),
+    death_date=patients.minimum_of(
+        "primary_care_death_date", "ons_died_from_any_cause_date"
+    ),
   
+  ###  COVID vaccination
+    # First covid vaccination date (first vaccine given on 8/12/2020 in the UK)
+    covid19_vaccination_date1=patients.with_tpp_vaccination_record(
+        # code for TPP only, when using patients.with_tpp_vaccination_record() function
+        target_disease_matches="SARS-2 CORONAVIRUS",
+        on_or_after="2020-12-07",
+        find_first_match_in_period=True,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "2020-12-08", "latest": "today"},
+            "incidence": 0.7
+        },
+    ),
+    # Second covid vaccination date (first second dose reported on 29/12/2020 in the UK)
+    covid19_vaccination_date2=patients.with_tpp_vaccination_record(
+        # code for TPP only, when using patients.with_tpp_vaccination_record() function
+        target_disease_matches="SARS-2 CORONAVIRUS",
+        on_or_after="covid19_vaccination_date1 + 14 days",  # Allowing for less days between 2 vaccination dates
+        find_first_match_in_period=True,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "2020-12-29", "latest": "today"},
+            "incidence": 0.6
+        },
+    ),
+
+# --- DEFINE COVARIATES ---
+
+  ### Sex
+  cov_sex = patients.sex(
+    return_expectations = {
+      "rate": "universal",
+      "category": {"ratios": {"M": 0.49, "F": 0.51}},
+    }
+  ),
+
   ### Age
-  age = patients.age_as_of(
+  cov_age = patients.age_as_of(
     "index_date",
     return_expectations = {
       "rate": "universal",
@@ -82,102 +205,153 @@ study = StudyDefinition(
       "incidence" : 0.001
     },
   ),
-  
-  ### Sex
-  sex = patients.sex(
-    return_expectations = {
-      "rate": "universal",
-      "category": {"ratios": {"M": 0.49, "F": 0.51}},
-    }
-  ),
-  
-### Index of multiple deprivation
-  imd = patients.categorised_as(
-    {"0": "DEFAULT",
-      "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
-      "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-      "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-      "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-      "5": """index_of_multiple_deprivation >= 32844*4/5 """,
-    },
-    index_of_multiple_deprivation = patients.address_as_of(
-      "index_date",
-      returning = "index_of_multiple_deprivation",
-      round_to_nearest = 100,
+
+  ### Ethnicity 
+        cov_ethnicity=patients.categorised_as(
+        helpers.generate_ethnicity_dictionary(16),
+        cov_ethnicity_sus=patients.with_ethnicity_from_sus(
+            returning="group_16", use_most_frequent_code=True
+        ),
+        cov_ethnicity_gp_opensafely=patients.with_these_clinical_events(
+            opensafely_ethnicity_codes_16,
+            on_or_before="index_date",
+            returning="category",
+            find_last_match_in_period=True,
+        ),
+        cov_ethnicity_gp_primis=patients.with_these_clinical_events(
+            primis_covid19_vacc_update_ethnicity,
+            on_or_before="index_date",
+            returning="category",
+            find_last_match_in_period=True,
+        ),
+        cov_ethnicity_gp_opensafely_date=patients.with_these_clinical_events(
+            opensafely_ethnicity_codes_16,
+            on_or_before="index_date",
+            returning="category",
+            find_last_match_in_period=True,
+        ),
+        cov_ethnicity_gp_primis_date=patients.with_these_clinical_events(
+            primis_covid19_vacc_update_ethnicity,
+            on_or_before="index_date",
+            returning="category",
+            find_last_match_in_period=True,
+        ),
+        return_expectations=helpers.generate_universal_expectations(16),
     ),
+
+  ###deprivation
+  cov_deprivation=patients.categorised_as(
+        helpers.generate_deprivation_ntile_dictionary(10),
+        index_of_multiple_deprivation=patients.address_as_of(
+            "index_date",
+            returning="index_of_multiple_deprivation",
+            round_to_nearest=100,
+        ),
+        return_expectations=helpers.generate_universal_expectations(10),
+    ),
+
+  ###Region
+  cov_region=patients.registered_practice_as_of(
+            "index_date",
+            returning="nuts1_region_name",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {
+                        "North East": 0.1,
+                        "North West": 0.1,
+                        "Yorkshire and The Humber": 0.1,
+                        "East Midlands": 0.1,
+                        "West Midlands": 0.1,
+                        "East": 0.1,
+                        "London": 0.2,
+                        "South East": 0.1,
+                        "South West": 0.1,
+                    },
+                },
+            },
+        ),
+
+
+#Medications
+
+  ###Smoking status
+    cov_smoking_status=patients.categorised_as(
+        {
+            "S": "most_recent_smoking_code = 'S'",
+            "E": """
+                 most_recent_smoking_code = 'E' OR (
+                   most_recent_smoking_code = 'N' AND ever_smoked
+                 )
+            """,
+            "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+            "M": "DEFAULT",
+        },
+        return_expectations={
+            "category": {"ratios": {"S": 0.6, "E": 0.1, "N": 0.2, "M": 0.1}}
+        },
+        most_recent_smoking_code=patients.with_these_clinical_events(
+            smoking_clear,
+            find_last_match_in_period=True,
+            on_or_before="index_date",
+            returning="category",
+        ),
+        ever_smoked=patients.with_these_clinical_events(
+            filter_codes_by_category(smoking_clear, include=["S", "E"]),
+            on_or_before="index_date",
+        ),
+    ),
+
+  ### BMI (to derive obesity binary variable)
+  cov_bmi = patients.with_these_clinical_events(
+    bmi_codes,
+    returning = "numeric_value",
+    ignore_missing_values = True,
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
     return_expectations = {
-      "rate": "universal",
-      "category": {
-        "ratios": {
-          "0": 0.01,
-          "1": 0.20,
-          "2": 0.20,
-          "3": 0.20,
-          "4": 0.20,
-          "5": 0.19,
-        }},
+      "float": {"distribution": "normal", "mean": 25, "stddev": 5},
     },
   ),
 
-
-
-
-
-# --- DEFINE COVID-19 EXPOSURE VARIABLES ---
-
-positive_covid_test_ever=patients.with_test_result_in_sgss(
-        pathogen="SARS-CoV-2",
-        test_result="positive",
-        on_or_after="index_date",
-        find_first_match_in_period=True,
-        returning="date",
-        date_format="YYYY-MM-DD",
+  ###No. primary care consultation in year prior to index date
+    cov_n_disorder=patients.with_gp_consultations(
+        between=["index_date - 12 months", "index_date"],
+        returning="number_of_matches_in_period",
         return_expectations={
-            "date": {"earliest": "index_date"},
-            "rate": "exponential_increase",
+            "int": {"distribution": "normal", "mean": 10, "stddev": 3},
+            "incidence": 1,
         },
     ),
 
-   covid_tpp_probable=patients.with_these_clinical_events(
-        combine_codelists(
-            covid_identification_in_primary_care_case_codes_clinical,
-            covid_identification_in_primary_care_case_codes_test,
-            covid_identification_in_primary_care_case_codes_seq,
-        ),
-        return_first_date_in_period=True,
-        include_day=True,
-        return_expectations={"date": {"earliest": "index_date"}, "incidence" : 0.95},
-    ), 
+#Diabetes
+#Any mental illness
+#Cancer
+#Chronic Obstructive Pulmonary Disorder
 
-   covid_tpp_codes_clinical=patients.with_these_clinical_events(
-        combine_codelists(covid_identification_in_primary_care_case_codes_clinical),
-        return_first_date_in_period=True,
-        include_day=True,
-        return_expectations={"date": {"earliest": "index_date"}, "incidence" : 0.5},
-    ), 
-
-   covid_tpp_codes_test=patients.with_these_clinical_events(
-        combine_codelists(covid_identification_in_primary_care_case_codes_test),
-        return_first_date_in_period=True,
-        include_day=True,
-        return_expectations={"date": {"earliest": "index_date"}, "incidence" : 0.5},
-    ), 
-
-   covid_tpp_codes_seq=patients.with_these_clinical_events(
-        combine_codelists(covid_identification_in_primary_care_case_codes_seq),
-        return_first_date_in_period=True,
-        include_day=True,
-        return_expectations={"date": {"earliest": "index_date"}, "incidence" : 0.5},
-    ), 
-  
-# --- DEFINE COVARIATES ---
-
-  ### Chronic liver disease
+  ###Liver disease (primary care)
   chronic_liver_disease = patients.with_these_clinical_events(
     chronis_liver_disease_codes,
     on_or_before = "index_date",
     returning = "binary_flag",
     return_expectations = {"incidence": 0.01},
   ),
+
+  #Major surgery
+#Hypertension
+#Dementia
+    dementia=patients.with_these_clinical_events(
+        dementia_codes,
+        on_or_before=f"{start_date}",
+        return_expectations={"incidence": 0.05},
+    ),
+#Antiplatelet medication
+#Lipid lowering medication
+#Anticoagulation medication
+#Combined oral contraceptive pill
+#Blood pressure lowering medication
+#Hormone replacement therapy
+
+
 
 )
