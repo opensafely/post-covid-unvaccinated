c0b920da69f900a3d76a053703db783f1ac307b7
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-04-25 13:30:00 +0100

    Update follow up periods

--- analysis/02_pipe.R
+++ analysis/02_pipe.R
@@ -44,7 +44,7 @@ cohort_end_date <- as.Date("2021-06-18")
 #Used to split time since COVID exposure; when there are time periods with no events then
 #a reduced number of time periods is used (need 197 instead of 196 as time periods are split using [ , ) 
 
-cuts_days_since_expo <- c(14, 28, 56, 84, 182, 366) 
+cuts_days_since_expo <- c(7, 14, 28, 56, 84, 182, 366) 
 cuts_days_since_expo_reduced <- c(28,366) 
 
 #Rename input variable names (by renaming here it means that these scripts can be used for other datasets without
