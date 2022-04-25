ad1e33a086d1b70691bdf0453557d45feca631af
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-04-25 13:43:36 +0100

    correct common vars

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -315,7 +315,7 @@ def generate_common_variables(index_date_variable):
 
     ###  Diabetes drugs
 
-    tmp_out_date_insulin_snomed=patients.with_these_clinical_events(
+    tmp_out_date_insulin_snomed=patients.with_these_medications(
         insulin_snomed,
         returning="date",
         between=["1990-01-01", "today"],
@@ -328,7 +328,7 @@ def generate_common_variables(index_date_variable):
         },
     ),
 
-    tmp_out_date_antidiabetic_drugs_snomed=patients.with_these_clinical_events(
+    tmp_out_date_antidiabetic_drugs_snomed=patients.with_these_medications(
         antidiabetic_drugs_snomed,
         returning="date",
         between=["1990-01-01", "today"],
@@ -346,7 +346,7 @@ def generate_common_variables(index_date_variable):
         "tmp_out_date_insulin_snomed","tmp_out_date_antidiabetic_drugs_snomed"
     ),
 
-    tmp_out_date_nonmetform_drugs_snomed=patients.with_these_clinical_events(
+    tmp_out_date_nonmetform_drugs_snomed=patients.with_these_medications(
         non_metformin_dmd,
         returning="date",
         between=["1990-01-01", "today"],
@@ -369,77 +369,6 @@ def generate_common_variables(index_date_variable):
          "tmp_out_date_diabetes_medication",
          "tmp_out_date_nonmetform_drugs_snomed"
     ),
-
-# #### **** ######
-#     ## Type 1 diabetes
-#     out_date_diabetes_type1=patients.with_these_clinical_events(
-#         diabetes_type1_snomed_clinical,
-#         returning="date",
-#         on_or_after=f"{index_date_variable}",
-#         date_format="YYYY-MM-DD",
-#         find_first_match_in_period=True,
-#         return_expectations={
-#             "date": {"earliest": "index_date", "latest" : "today"},
-#             "rate": "uniform",
-#             "incidence": 0.1,
-#         },
-#     ),
-
-#     ## Type 2 diabetes
-#     out_date_diabetes_type2=patients.with_these_clinical_events(
-#         diabetes_type2_snomed_clinical,
-#         returning="date",
-#         on_or_after=f"{index_date_variable}",
-#         date_format="YYYY-MM-DD",
-#         find_first_match_in_period=True,
-#         return_expectations={
-#             "date": {"earliest": "index_date", "latest" : "today"},
-#             "rate": "uniform",
-#             "incidence": 0.1,
-#         },
-#     ),
-
-#     ## Other or non-diagnostic diabetes
-#     out_date_diabetes_nond=patients.with_these_clinical_events(
-#         diabetes_diagnostic_snomed,
-#         returning="date",
-#         on_or_after=f"{index_date_variable}",
-#         date_format="YYYY-MM-DD",
-#         find_first_match_in_period=True,
-#         return_expectations={
-#             "date": {"earliest": "index_date", "latest" : "today"},
-#             "rate": "uniform",
-#             "incidence": 0.1,
-#         },
-#     ),
-
-#     ## Other or non-specific diabetes
-#     out_date_diabetes_other=patients.with_these_clinical_events(
-#         diabetes_other_snomed_clinical,
-#         returning="date",
-#         on_or_after=f"{index_date_variable}",
-#         date_format="YYYY-MM-DD",
-#         find_first_match_in_period=True,
-#         return_expectations={
-#             "date": {"earliest": "index_date", "latest" : "today"},
-#             "rate": "uniform",
-#             "incidence": 0.1,
-#         },
-#     ),
-
-#     ## Gestational diabetes
-#     out_date_diabetes_gestational=patients.with_these_clinical_events(
-#         diabetes_gestational_snomed_clinical,
-#         returning="date",
-#         on_or_after=f"{index_date_variable}",
-#         date_format="YYYY-MM-DD",
-#         find_first_match_in_period=True,
-#         return_expectations={
-#             "date": {"earliest": "index_date", "latest" : "today"},
-#             "rate": "uniform",
-#             "incidence": 0.1,
-#         },
-#     ),
 
     ## MENTAL HEALTH OUTCOMES -------------------
 
@@ -1313,49 +1242,34 @@ def generate_common_variables(index_date_variable):
         "tmp_cov_bin_hypertension_snomed", "tmp_cov_bin_hypertension_hes", "cov_bin_hypertension_drugs_dmd",
     ),
 
-    # ## Diabetes
-    # ### Primary care
-    # tmp_cov_bin_diabetes_snomed=patients.with_these_clinical_events(
-    #     diabetes_snomed_clinical,
-    #     returning='binary_flag',
-    #     on_or_before=f"{index_date_variable}",
-    #     return_expectations={"incidence": 0.1},
-    # ),
-    # ### HES APC
-    # tmp_cov_bin_diabetes_hes=patients.admitted_to_hospital(
-    #    returning='binary_flag',
-    #    with_these_diagnoses=diabetes_icd10,
-    #    on_or_before=f"{index_date_variable}",
-    #    return_expectations={"incidence": 0.1},
-    # ),
-    # ### DMD
-    # tmp_cov_bin_diabetes_dmd=patients.with_these_clinical_events(
-    #     diabetes_drugs_dmd,
-    #     returning='binary_flag',
-    #     on_or_before=f"{index_date_variable}",
-    #     return_expectations={"incidence": 0.1},
-    # ),
-    # ### Combined
-    # cov_bin_diabetes = patients.maximum_of(
-    #     "tmp_cov_bin_diabetes_snomed", "tmp_cov_bin_diabetes_dmd", "tmp_cov_bin_diabetes_snomed",
-    # ),
-
-    ## Type 1 diabetes
-    cov_bin_diabetes_type1=patients.with_these_clinical_events(
+    ## Type 1 diabetes primary care
+    cov_bin_diabetes_type1_snomed=patients.with_these_clinical_events(
         diabetes_type1_snomed,
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.1},
     ),
-
-    ## Type 2 diabetes
-    cov_bin_diabetes_type2=patients.with_these_clinical_events(
+    ## Type 1 diabetes HES
+    cov_bin_diabetes_type1_hes=patients.admitted_to_hospital(
+       returning='binary_flag',
+       with_these_diagnoses=diabetes_type1_icd10,
+       on_or_before=f"{index_date_variable}",
+       return_expectations={"incidence": 0.1},
+    ),
+    ## Type 2 diabetes primary care
+    cov_bin_diabetes_type2_snomed=patients.with_these_clinical_events(
         diabetes_type2_snomed,
         returning='binary_flag',
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.1},
     ),
-
+    ## Type 2 diabetes HES
+    cov_bin_diabetes_type2_hes=patients.admitted_to_hospital(
+       returning='binary_flag',
+       with_these_diagnoses=diabetes_type2_icd10,
+       on_or_before=f"{index_date_variable}",
+       return_expectations={"incidence": 0.1},
+    ),
     ## Other or non-specific diabetes
     cov_bin_diabetes_other=patients.with_these_clinical_events(
         diabetes_other_snomed,
@@ -1363,7 +1277,6 @@ def generate_common_variables(index_date_variable):
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.1},
     ),
-
     ## Gestational diabetes
     cov_bin_diabetes_gestational=patients.with_these_clinical_events(
         diabetes_gestational_snomed,
@@ -1371,10 +1284,28 @@ def generate_common_variables(index_date_variable):
         on_or_before=f"{index_date_variable}",
         return_expectations={"incidence": 0.1},
     ),
+    ## Diabetes medication
+    tmp_cov_bin_insulin_snomed=patients.with_these_medications(
+        insulin_snomed,
+        returning="binary_flag",
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.05},
+    ),
 
-    ## Any diabetes
+    tmp_cov_bin_antidiabetic_drugs_snomed=patients.with_these_medications(
+        antidiabetic_drugs_snomed,
+        returning="binary_flag",
+        on_or_before=f"{index_date_variable}",
+        return_expectations={"incidence": 0.05},
+    ),
+
+    ## Any diabetes covariate
     cov_bin_diabetes=patients.maximum_of(
-        "cov_bin_diabetes_type1", "cov_bin_diabetes_type2", "cov_bin_diabetes_other", "cov_bin_diabetes_gestational",
+        "cov_bin_diabetes_type1_snomed", "cov_bin_diabetes_type1_hes", 
+        "cov_bin_diabetes_type2_snomed", "cov_bin_diabetes_type2_hes",
+        "cov_bin_diabetes_other", 
+        "cov_bin_diabetes_gestational",
+        "tmp_cov_bin_insulin_snomed", "tmp_cov_bin_antidiabetic_drugs_snomed"
     ),
 
     ## Prediabetes
