b7dd9fa1ef636b04905b6157fc50027fe7965fc9
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-05-10 15:09:11 +0100

    combine to create one obesity variable

--- analysis/preprocess_data.R
+++ analysis/preprocess_data.R
@@ -38,6 +38,11 @@ df$cov_num_tc_hdl_ratio[is.infinite(df$cov_num_tc_hdl_ratio)] <- NA
 
 print("Diabetes count variables created successfully")
 
+#Combine BMI variables to create one history of obesity variable ---------------
+
+df$cov_bin_obesity <-ifelse(df$cov_bin_obesity==TRUE |df$cov_cat_bmi_groups=="Obese",TRUE,FALSE)
+df[,c("cov_num_bmi")] <- NULL
+
 # Format columns -----------------------------------------------------
 # dates, numerics, factors, logicals
 
