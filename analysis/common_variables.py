eef6c408ad22617112d436fb20a40a1e21851fa7
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-05-12 15:19:19 +0100

    Update common_variables.py

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -1041,7 +1041,7 @@ def generate_common_variables(index_date_variable):
         "tmp_cov_bin_stroke_isch_hes", "tmp_cov_bin_stroke_isch_snomed", "tmp_cov_bin_stroke_sah_hs_hes", "tmp_cov_bin_stroke_sah_hs_snomed",
     ),
 
-    ### Combined
+    ### Combined Stroke Ischeamic
     cov_bin_stroke_isch=patients.maximum_of(
         "tmp_cov_bin_stroke_isch_hes", "tmp_cov_bin_stroke_isch_snomed",
     ),
