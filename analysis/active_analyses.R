114a6383df5a92c6e0c37e460398981f864f1d1f
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-05-16 16:23:33 +0100

    fix error and update yaml

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -44,7 +44,7 @@ outcomes_short <- c("t1dm","t2dm","otherdm","gestationaldm")
 outcome_venn <- c(TRUE, TRUE, FALSE, FALSE)
 
 for (i in 1:length(outcomes)) {
-  df[nrow(df)+1,] <- c(FALSE,
+  df[nrow(df)+1,] <- c(TRUE,
                        outcomes[i],
                        outcome_group,
                        paste0("out_date_",outcomes_short[i]),
@@ -90,7 +90,7 @@ outcomes_short <- c("depression",
 out_venn <- c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
 
 for (i in 1:length(outcomes)) {
-  df[nrow(df)+1,] <- c(FALSE,
+  df[nrow(df)+1,] <- c(TRUE,
                        outcomes[i],
                        outcome_group,
                        paste0("out_date_",outcomes_short[i]),
@@ -102,7 +102,7 @@ for (i in 1:length(outcomes)) {
                        out_venn[i])
 }
 
-df[6,1] <- TRUE
+# df[6,1] <- TRUE
 
 # Save active analyses list ----------------------------------------------------
 
