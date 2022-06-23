e5928705ef287cbbef71269c3e4a93931168f043
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-06-23 16:44:10 +0100

    Deactivate diabetes.
    Updated yaml, to run mental health analyses.

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -41,7 +41,7 @@ outcomes <- c("type 1 diabetes",
 outcome_group <- "diabetes"
 
 outcomes_short <- c("t1dm","t2dm","otherdm","gestationaldm")
-outcome_venn <- c(TRUE, TRUE, FALSE, FALSE)
+outcome_venn <- c(FALSE, FALSE, FALSE, FALSE)
 
 for (i in 1:length(outcomes)) {
   df[nrow(df)+1,] <- c(FALSE,
@@ -50,7 +50,7 @@ for (i in 1:length(outcomes)) {
                        paste0("out_date_",outcomes_short[i]),
                        "cov_cat_sex;cov_num_age;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_num_consulation_rate;cov_cat_smoking_status;cov_bin_ami;cov_bin_all_stroke;cov_bin_other_arterial_embolism;cov_bin_vte;cov_bin_hf;cov_bin_angina;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_depression;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_healthcare_worker;cov_bin_carehome_status;cov_num_tc_hdl_ratio;cov_cat_bmi_groups;cov_bin_prediabetes;cov_bin_diabetes_gestational",
                        rep("all",1),
-                       rep(TRUE,3),
+                       rep(FALSE,3),
                        rep(FALSE,14),
                        "",
                        outcome_venn[i])
@@ -69,8 +69,8 @@ df <- df %>% mutate(covariates = case_when(outcome_variable == "out_date_gestati
 # add pre diabetes subgroup analysis
 
 df$prior_history_var <- ifelse(df$outcome=="type 2 diabetes" ,"cov_bin_prediabetes",df$prior_history_var)
-df$prior_history_TRUE <- ifelse(df$outcome=="type 2 diabetes" ,TRUE,df$prior_history_TRUE)
-df$prior_history_FALSE <- ifelse(df$outcome=="type 2 diabetes" ,TRUE,df$prior_history_FALSE)
+df$prior_history_TRUE <- ifelse(df$outcome=="type 2 diabetes" ,FALSE,df$prior_history_TRUE)
+df$prior_history_FALSE <- ifelse(df$outcome=="type 2 diabetes" ,FALSE,df$prior_history_FALSE)
 
 # Add mental health outcomes --------------------------------------------------------
 
