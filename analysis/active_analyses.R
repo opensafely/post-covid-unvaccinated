34b8b37951565cf8b4f08cca21c8401d0b6f5da5
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-06-30 10:59:53 +0100

    MH Analysis
    This branch can run: main, hospitalised, and non-hospitalised analysis and subgroups for MH.
    Prescriptions for anxiolytics, opioid, and antidepressants were added.

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -47,16 +47,16 @@ outcomes <- c("type 1 diabetes",
 outcome_group <- "diabetes"
 
 outcomes_short <- c("t1dm","t2dm", "t2dm_pd","t2dm_pd_no", "t2dm_obes","t2dm_obes_no", "otherdm","gestationaldm")
-outcome_venn <- c(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+outcome_venn <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
 
 for (i in 1:length(outcomes)) {
-  df[nrow(df)+1,] <- c(TRUE,
+  df[nrow(df)+1,] <- c(FALSE,
                        outcomes[i],
                        outcome_group,
                        paste0("out_date_",outcomes_short[i]),
                        "cov_cat_sex;cov_num_age;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_num_consulation_rate;cov_cat_smoking_status;cov_bin_ami;cov_bin_all_stroke;cov_bin_other_arterial_embolism;cov_bin_vte;cov_bin_hf;cov_bin_angina;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_depression;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_healthcare_worker;cov_bin_carehome_status;cov_num_tc_hdl_ratio;cov_cat_bmi_groups;cov_bin_prediabetes;cov_bin_diabetes_gestational",
                        rep("all",1),
-                       rep(TRUE,3),
+                       rep(FALSE,3),
                        rep(FALSE,14),
                        "",
                        outcome_venn[i])
@@ -80,7 +80,7 @@ df <- df %>% mutate(outcome_group = case_when(outcome == "type 2 diabetes - pre 
 
 # turn on subgroups for main t2dm analyses
 
-df[2,c(10:21)] <- TRUE
+df[2,c(10:21)] <- FALSE
 
 # Remove sex as a covariate for gestational diabetes analysis
 
@@ -140,14 +140,14 @@ outcomes_short <- c("depression",
 out_venn <- c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
 
 for (i in 1:length(outcomes)) {
-  df[nrow(df)+1,] <- c(FALSE,
+  df[nrow(df)+1,] <- c(TRUE,
                        outcomes[i],
                        outcome_group,
                        paste0("out_date_",outcomes_short[i]),
                        "cov_num_age;cov_cat_sex;cov_cat_ethnicity;cov_cat_deprivation;cov_cat_region;cov_cat_smoking_status;cov_bin_carehome_status;cov_num_consulation_rate;cov_bin_healthcare_worker;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_ami;cov_bin_stroke_isch;cov_bin_recent_depression;cov_bin_history_depression;cov_bin_recent_anxiety;cov_bin_history_anxiety;cov_bin_recent_eating_disorders;cov_bin_history_eating_disorders;cov_bin_recent_serious_mental_illness;cov_bin_history_serious_mental_illness;cov_bin_recent_self_harm;cov_bin_history_self_harm",
                        rep("all",1),
-                       rep(TRUE,1),
-                       rep(FALSE,16),
+                       rep(TRUE,3),
+                       rep(FALSE,14),
                        "",
                        out_venn[i])
 }
